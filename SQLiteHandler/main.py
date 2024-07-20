8e5c98d6cf0ba5e5ce0f3595959fc982d6a3d8ac
Sebair <sebair_selmani@yahoo.com>
2024-07-20 17:52:27 +0200

    update main.py in SQLite handler

--- SQLiteHandler/main.py
+++ SQLiteHandler/main.py
@@ -1,34 +1,40 @@
-import requests
-import sqlite3
-import pandas as pd
+import sqlitecloud
 from config import sConnString
 
 class SQLiteHandler():
     def __init__(self):
-        self.engine = sqlite3.connect(sConnString)
+        self.engine = sqlitecloud.connect(sConnString)
+        self.engine.execute('USE database Bills')
     def getAllBills(self):
         lRes = self.engine.execute('select * from Bills')
         return lRes
     def addBill(self,values:dict):
-        sql = f''' INSERT INTO Bills (ID,UserID,RecivierID,Amount,Period,PaidAt,Description,CardID,Tag,FileID)
+        sql = f''' INSERT OR IGNORE INTO Bills
                      VALUES(?,?,?,?,?,?,?,?,?,?) '''
         self.engine.execute(sql, values)
         self.engine.commit()
-    def addCard(self,values):
-        sql = f''' INSERT INTO Cards (ID,UserID,RecivierID,Amount,Period,PaidAt,Description,CardID,Tag,FileID)
-                       VALUES(?,?,?,?,?,?,?,?,?,?) '''
-        self.engine.execute(sql, values)
+
+    def addCard(self,values:tuple):
+        sql = f''' insert into Cards values (?,?,?,?)'''
+        self.engine.execute(sql,values)
+        self.engine.commit()
+    def addRecivier(self,values:tuple):
+        sql = ''' insert or ignore into Reciviers values (?,?,?,?,?,?)'''
+        self.engine.execute(sql,values)
+        self.engine.commit()
+    def addUser(self,values:tuple):
+        sql = ''' insert or ignore into Users values (?,?,?,?,?)'''
+        self.engine.execute(sql,values)
         self.engine.commit()
 
 
 
 if __name__=='__main__':
     h = SQLiteHandler()
-    values = (
-        1,
-        1,
-        1,
+    tCardValues = (1,'DebitCard','SparkasseBankFrankenmarkt','ATSomeOther')
+    tRecivierValues = (1,'MaxEnergy','ElectricityProvider','Electricity','BankToBeUpdated','ATMaxEnergy')
+    tUsersValues = (1,'Sebair','sebair_selmani@yahoo.com','pass','20240720')
+    tBillValues = (2,25,'July','June','BillForJuly','Electricity',1,1,1,1)
 
 
-    )
-    h.addBill()
+    h.addBill(tBillValues)
